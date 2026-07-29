9594d1d7fe1051226b86ebbe52e0103eda78410c
Anastasiia <nastyadedela@gmail.com>
2026-07-29 20:15:09 +0200

    feat: add desktop version of data centers page

--- src/app/(site)/data-centers/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function DataCentersPage() {
-  return (
-    <section className="mx-auto w-full max-w-340 px-5 py-24">
-      <h1 className="text-3xl font-extrabold tracking-tight md:text-4xl">Data Centers</h1>
-    </section>
-  );
-}
