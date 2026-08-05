c680deb870684ec66e273ab2277736b0c96801c6
Anastasiia <nastyadedela@gmail.com>
2026-08-05 18:28:47 +0200

    feat: make what-is-vps img animated

--- src/components/home/WhatIsVps.tsx
+++ src/components/home/WhatIsVps.tsx
@@ -1,5 +1,5 @@
-import Image from 'next/image';
 import { Button } from '@/components/ui/button';
+import { VpsServerScene } from '@/components/home/VpsServerScene';
 
 export function WhatIsVps() {
   return (
@@ -8,13 +8,7 @@ export function WhatIsVps() {
         <div className="order-2 mx-auto flex w-full max-w-[514px] flex-1 items-center justify-center pb-19 md:order-1 md:mx-0 md:pb-0">
           <div className="relative mx-auto w-full">
             <div className="absolute bottom-0 left-0 aspect-[514/215] w-full translate-y-[76px] rounded-[20px] bg-[linear-gradient(180deg,rgba(67,76,247,0)_0%,rgba(67,76,247,0.16)_100%)]" />
-            <Image
-              src="/images/what-is-vps.png"
-              alt="Isometric illustration of a VPS server node"
-              width={716}
-              height={552}
-              className="relative z-10 mx-auto block h-auto w-full max-w-[358px]"
-            />
+            <VpsServerScene className="relative z-10 mx-auto w-full max-w-[358px]" />
           </div>
         </div>
 
